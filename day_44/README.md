6b667e938cea18fde04254bc7b565385433153ca
Ismael Gorissen <ismael.gorissen@gmail.com>
2024-09-18 15:42:22 +0200

    feat: complete day 44

---
+++ day_44/README.md
@@ -0,0 +1,5 @@
+# Project 9, part two
+
+[⬅ DAY 43](../day_43) | [..](../) | [DAY 45 ➡](../day_45)
+
+[https://www.hackingwithswift.com/100/swiftui/44](https://www.hackingwithswift.com/100/swiftui/44)
--- projects/Navigation/Navigation/ContentView.swift
+++ projects/Navigation/Navigation/ContentView.swift
@@ -32,25 +32,144 @@ import SwiftUI
 //    }
 //}
 
-struct Student: Hashable {
-    var id = UUID()
-    var name: String
-    var age: Int
+//struct Student: Hashable {
+//    var id = UUID()
+//    var name: String
+//    var age: Int
+//}
+//
+//struct ContentView: View {
+//    var body: some View {
+//        NavigationStack {
+//            List(0..<100) { i in
+//                NavigationLink("Select \(i)", value: i)
+//            }
+//            .navigationDestination(for: Int.self) { selection in
+//                Text("You selected \(selection)")
+////                DetailView(number: selection)
+//            }
+////            .navigationDestination(for: Student.self) { student in
+////                Text("Student \(student.name) - \(student.id)")
+////            }
+//        }
+//    }
+//}
+
+//struct ContentView: View {
+//    @State private var path = [Int]()
+//    
+//    var body: some View {
+//        NavigationStack(path: $path) {
+//            VStack {
+//                Button("Show 32") {
+//                    path = [32]
+//                }
+//                
+//                Button("Show 64") {
+//                    path.append(64)
+//                }
+//                
+//                Button("Show 32, then 64") {
+//                    path = [32, 64]
+//                }
+//            }
+//            .navigationDestination(for: Int.self) { selection in
+//                Text("You selected \(selection)")
+//            }
+//        }
+//    }
+//}
+
+//struct ContentView: View {
+//    @State private var path = NavigationPath()
+//    
+//    var body: some View {
+//        NavigationStack(path: $path) {
+//            List {
+//                ForEach(0..<5) { i in
+//                    NavigationLink("Select Number \(i)", value: i)
+//                }
+//                
+//                ForEach(0..<5) { i in
+//                    NavigationLink("Select String \(i)", value: String(i))
+//                }
+//            }
+//            .toolbar {
+//                Button("Push 556") {
+//                    path.append(556)
+//                }
+//                
+//                Button("Push Hello") {
+//                    path.append("Hello")
+//                }
+//            }
+//            .navigationDestination(for: Int.self) { selection in
+//                Text("You selected the number \(selection)")
+//            }
+//            .navigationDestination(for: String.self) { selection in
+//                Text("You selected the string \(selection)")
+//            }
+//        }
+//    }
+//}
+
+@Observable
+class PathStore {
+    var path: NavigationPath {
+        didSet {
+            save()
+        }
+    }
+    
+    private let savePath = URL.documentsDirectory.appending(path: "SavedPath")
+    
+    init() {
+        if let data = try? Data(contentsOf: savePath) {
+            if let decoded = try? JSONDecoder().decode(NavigationPath.CodableRepresentation.self, from: data) {
+                path = NavigationPath(decoded)
+                return
+            }
+        }
+        
+        path = NavigationPath()
+    }
+    
+    func save() {
+        guard let representation = path.codable else { return }
+        
+        do {
+            let data = try JSONEncoder().encode(representation)
+            try data.write(to: savePath)
+        } catch {
+            print("Failed to save navigation data")
+        }
+    }
+}
+
+struct DetailView: View {
+    let number: Int
+    @Binding var path: NavigationPath
+    
+    var body: some View {
+        NavigationLink("Go to random number", value: Int.random(in: 1...1_000))
+            .navigationTitle("Number: \(number)")
+            .toolbar {
+                Button("Home") {
+                    path = NavigationPath()
+                }
+            }
+    }
 }
 
 struct ContentView: View {
+    @State private var pathStore = PathStore()
+    
     var body: some View {
-        NavigationStack {
-            List(0..<100) { i in
-                NavigationLink("Select \(i)", value: i)
-            }
-            .navigationDestination(for: Int.self) { selection in
-                Text("You selected \(selection)")
-//                DetailView(number: selection)
-            }
-//            .navigationDestination(for: Student.self) { student in
-//                Text("Student \(student.name) - \(student.id)")
-//            }
+        NavigationStack(path: $pathStore.path) {
+            DetailView(number: 0, path: $pathStore.path)
+                .navigationDestination(for: Int.self) { i in
+                    DetailView(number: i, path: $pathStore.path)
+                }
         }
     }
 }
